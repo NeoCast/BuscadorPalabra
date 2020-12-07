77a0660029ec7d8d3936d8e0738998ab62043c3d
NeoCast <42457576+NeoCast@users.noreply.github.com>
2020-12-07 18:35:28 -0300

    Se crea respositorio

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var programa = new Program();
+            Console.WriteLine("Escriba una cadena:");
+            var entrada = Console.ReadLine();
+            programa.mostrarCadena(entrada);
+        }
+
+        public string mostrarCadena(string cadena)
+        {
+
+           
+            string[] palabras = cadena.Split(' ');
+            int total;
+            Array.Sort(palabras);
+            foreach (var palabra in palabras)
+            {
+                total = Regex.Matches(cadena, palabra).Count;
+                System.Console.WriteLine($"Palabra: <{palabra}>, cantidad que repite: {total}");
+            }
+            return "";
+
+        }
+
+
+    }
+}
